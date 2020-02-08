807227f833fc28e3619b837fc816f0338ee21abe
kodecamp <kodecamp.in@gmail.com>
2020-02-08 13:35:10 +0530

    updated with junit 5

---
+++ src/test/java/in/kodecamp/AppTest.java
@@ -0,0 +1,18 @@
+package in.kodecamp;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import java.util.List;
+import org.junit.jupiter.api.Test;
+
+/**
+ * Unit test for simple App.
+ */
+public class AppTest {
+
+  @Test
+  public void testApp() {
+    System.out.println("Hello World");
+    System.out.println("Done Hello World");
+    assertEquals(5, 6, "");
+  }
+}
--- watchandrun.sh
+++
@@ -1,3 +0,0 @@
-#!/bin/bash
-
-find src/ | entr sh -c 'mvn -q package exec:java'
